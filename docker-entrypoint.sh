cd20563552ae36b625429dec5369d8bd0410b415
James Lin <james.lin1997@hotmail.com>
2020-10-15 01:58:37 +1100

    fix ros dockerfile (still needs some testing); remove expose from web dockerfile; add entrypoint script and run script

---
+++ docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# exit immediately if a command exits with a non-zero status (see `$ help set`)
+set -e
+
+# location of master
+# export ROS_MASTER_URI=http://l:11311/
+# echo "$ECHO_PREFIX" "set ROS master: " "$ROS_MASTER_URI"
+
+# ROS installation
+source /opt/ros/kinetic/setup.bash
+
+# workspace holding custom ROS packages
+source /root/catkin_ws/devel/setup.bash
+
+echo "$ECHO_PREFIX" "call" "$@"
+exec "$@"
