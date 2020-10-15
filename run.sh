13020e907a7361e2f356da44e2b19c764d38524f
James Lin <james.lin1997@hotmail.com>
2020-10-16 01:50:55 +1100

    fix gazebo gui launching via docker; fix web dockerization issue where node_modules relies on host when it shouldnt; changes to dockerignore and run scripts

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
+xhost +
+
+# DISPLAY=$DISPLAY allows container to connect to X server
+# QT_X11_NO_MITSHM=1 stops Qt from using MIT-SHM X11 extension
+# Volume is also needed to connect to X server
+# GPUs all allows container to access host's GPU (unfortunately Gazebo requires GPU to launch properly)
 docker run -it \
-    --name="ur5_t2_4230_app_test" \
-    --env="DISPLAY=$IP:0" \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    ur5_app:latest
+    --gpus="all" \
+    test-gazebo:latest \
+    roslaunch ur5_t2_4230 ur5_world.launch
